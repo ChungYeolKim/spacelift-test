e729e054a8780bc579eb8a33e9d6ccc2389b28a4
ChungYeolKim <41099007+ChungYeolKim@users.noreply.github.com>
2024-07-02 14:56:23 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "github" {
+  owner = "kimcy12"
+}
+
+resource "github_repository" "my_repo" {
+  name        = "tf_sp_example"
+  description = "Spacelift configuration examples"
+
+  visibility = "public"
+}
